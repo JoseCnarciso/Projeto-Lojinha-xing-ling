238a3d0d3d5586905902439bcfdcc1e089666bc2
joseCnarciso <josecdia@hotmail.com>
2023-08-26 21:58:44 -0300

    Exercicio feito em aula

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
+<template>
+  <v-layout>
+    <v-app-bar color="#d6249f">
+      <v-app-bar-title>Funko Lab</v-app-bar-title>
 
-<template>  
+      <v-btn icon>
+        <router-link to="/carrinho">
+        {{ this.$store.state.produtosCarrinho.length}}
+          <v-icon color="#FFF">mdi-cart</v-icon>
+        </router-link>
+      </v-btn>
+    </v-app-bar>
 
-  <RouterView />
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-layout>
 </template>
-
-<script >
-
-import { RouterLink, RouterView } from 'vue-router'
-
-</script>
--- src/main.js
+++ src/main.js
@@ -9,6 +9,9 @@ import '@mdi/font/css/materialdesignicons.css'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import store from './store/index.js'
+
+
 
 const vuetify = createVuetify({
   components,
@@ -21,6 +24,7 @@ const vuetify = createVuetify({
 const app = createApp(App)
 
 app.use(router)
+app.use(store)
 app.use(vuetify)
 
 app.mount('#app')
